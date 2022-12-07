d997dbb23848d8ed2d78cb11cf8c529b8cc91ff6
Luke <luke.perkin@gmail.com>
2022-12-07 21:15:09 +0000

    Update day06.zig

--- src/day06.zig
+++ src/day06.zig
@@ -1,47 +1,70 @@
 const std = @import("std");
-const Allocator = std.mem.Allocator;
-const List = std.ArrayList;
-const Map = std.AutoHashMap;
-const StrMap = std.StringHashMap;
-const BitSet = std.DynamicBitSet;
-
-const util = @import("util.zig");
-const gpa = util.gpa;
-
+const print = std.debug.print;
 const data = @embedFile("data/day06.txt");
 
-pub fn main() !void {
-    
+fn RingBuffer(comptime T: type, comptime N: usize) type {
+    return struct {
+        items: [N]T = undefined,
+        capacity: usize = N,
+        cursor: usize = 0,
+        put_count: usize = 0,
+        const Self = @This();
+
+        pub fn next(self: *Self) T {
+            const result: T = self.items[self.cursor];
+            self.cursor += 1;
+            if (self.cursor == N) {
+                self.cursor = 0;
+            }
+            return result;
+        }
+
+        pub fn put(self: *Self, item: T) void {
+            self.items[self.cursor] = item;
+            self.put_count += 1;
+            _ = self.next();
+        }
+
+        pub fn get(self: Self) T {
+            return self.items[self.cursor];
+        }
+    };
 }
 
-// Useful stdlib functions
-const tokenize = std.mem.tokenize;
-const split = std.mem.split;
-const indexOf = std.mem.indexOfScalar;
-const indexOfAny = std.mem.indexOfAny;
-const indexOfStr = std.mem.indexOfPosLinear;
-const lastIndexOf = std.mem.lastIndexOfScalar;
-const lastIndexOfAny = std.mem.lastIndexOfAny;
-const lastIndexOfStr = std.mem.lastIndexOfLinear;
-const trim = std.mem.trim;
-const sliceMin = std.mem.min;
-const sliceMax = std.mem.max;
+fn doesSliceContainDuplicate(comptime T: type, items: []T) bool {
+    for (items) |item_0, i_0| {
+        for (items) |item_1, i_1| {
+            if (i_0 == i_1) continue;
+            if (item_0 == item_1) {
+                return true;
+            }
+        }       
+    }
+    return false;
+}
 
-const parseInt = std.fmt.parseInt;
-const parseFloat = std.fmt.parseFloat;
+fn findMarkerInByteStream(stream_buffer: anytype, comptime print_msg: []const u8) void {
+    for (data) |byte, byte_i| {
+        stream_buffer.put(byte);
+        if (stream_buffer.put_count >= stream_buffer.capacity) {
+            var b_found_same = doesSliceContainDuplicate(u8, stream_buffer.items[0..]);
+            if (!b_found_same) {
+                print("{s}\n", .{print_msg});
+                print("Found sequence with no duplicate starting at index: {d}.\n", .{byte_i});
+                print("Processed {d} bytes.\n", .{stream_buffer.put_count});
+                break;
+            }
+        }
+    }
+}
 
-const min = std.math.min;
-const min3 = std.math.min3;
-const max = std.math.max;
-const max3 = std.math.max3;
+const PacketMarkerRingBuffer = RingBuffer(u8, 4);
+const MessageMarkerRingBuffer = RingBuffer(u8, 14);
 
-const print = std.debug.print;
-const assert = std.debug.assert;
+pub fn main() !void {
+    var packet_marker_stream_buffer = PacketMarkerRingBuffer {};
+    findMarkerInByteStream(&packet_marker_stream_buffer, "start-of-packet marker...");
 
-const sort = std.sort.sort;
-const asc = std.sort.asc;
-const desc = std.sort.desc;
-
-// Generated from template/template.zig.
-// Run `zig build generate` to update.
-// Only unmodified days will be updated.
+    var message_marker_stream_buffer = MessageMarkerRingBuffer {};
+    findMarkerInByteStream(&message_marker_stream_buffer, "start-of-message marker...");
+}
